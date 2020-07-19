353d845cd4fe936a328bdb6b9e440f3faf5ad459
adurant101 <adurant101@gmail.com>
2020-07-18 20:18:24 -0700

    no login in chat

---
+++ no login in chat sesh/JavaChatClient222/src/javachat2/Chat2UserInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javachat2;
+
+/**
+ *
+ * @author pyaks
+ */
+public interface Chat2UserInterface {
+    
+    void processCommands();
+    
+}
